9e3440f40512d69d4e10212ee9b68ebb9b8c55c7
Frank Lechner <mail@franklechner.de>
2019-07-14 13:51:12 +0200

    configure jest

--- src/store/actions/index.test.ts
+++ src/store/actions/index.test.ts
@@ -1,24 +1,34 @@
+import configureMockStore from "redux-mock-store";
+import fetchMock from "fetch-mock";
+import thunk from "redux-thunk";
 import * as actions from ".";
-import {
-  FetchStart,
-  FetchError,
-  FetchSuccess,
-  FetchReset,
-  FETCH_START,
-  FETCH_SUCCESS,
-  FETCH_ERROR,
-  FETCH_RESET
-} from "../actions";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
 
 describe("sync actions", () => {
-  it("should create an action to set isLoading for reports fetch", () => {
-    const expectedAction: FetchStart = {
-      type: FETCH_START,
-      location: "reports"
-    };
-    expect(actions.fetchStart("reports")).toEqual(expectedAction);
+  afterEach(() => {
+    fetchMock.restore();
   });
 
+  /*   it("creates FETCH_TODOS_SUCCESS when fetching todos has been done", () => {
+    fetchMock.getOnce("/somepath", {
+      body: { todos: ["do something"] },
+      headers: { "content-type": "application/json" }
+    });
+
+    const expectedActions = [
+      { type: actions.FETCH_START },
+      { type: actions.FETCH_SUCCESS, body: { todos: ["do something"] } }
+    ];
+    const store = mockStore({ todos: [] });
+
+    return store.dispatch(actions.fetchData("reports")).then(() => {
+      // return of async actions
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+ */
   /*   it("should create an action to set success for reports fetch", () => {
     const expectedAction: FetchSuccess = {
       type: FETCH_SUCCESS,
@@ -28,17 +38,25 @@ describe("sync actions", () => {
     expect(actions.fetchSuccess("reports")).toEqual(expectedAction);
   }); */
 
+  it("should create an action to set isLoading for reports fetch", () => {
+    const expectedAction: actions.FetchStart = {
+      type: actions.FETCH_START,
+      location: "reports"
+    };
+    expect(actions.fetchStart("reports")).toEqual(expectedAction);
+  });
+
   it("should create an action to set hasError for reports fetch", () => {
-    const expectedAction: FetchError = {
-      type: FETCH_ERROR,
+    const expectedAction: actions.FetchError = {
+      type: actions.FETCH_ERROR,
       location: "reports"
     };
     expect(actions.fetchError("reports")).toEqual(expectedAction);
   });
 
   it("should create an action to reset a fetching process for reports fetch", () => {
-    const expectedAction: FetchReset = {
-      type: FETCH_RESET,
+    const expectedAction: actions.FetchReset = {
+      type: actions.FETCH_RESET,
       location: "reports"
     };
     expect(actions.fetchReset("reports")).toEqual(expectedAction);
