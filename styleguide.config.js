9c163c5d90b5bc89aa434afb176d43a312feadac
Frank Lechner <mail@franklechner.de>
2019-07-19 13:59:05 +0200

    fix styleguide aria props issue

--- styleguide.config.js
+++ styleguide.config.js
@@ -3,9 +3,21 @@ const path = require("path");
 module.exports = {
   title: "Nova Demo",
   components: "src/components/**/*.tsx",
-  propsParser: require("react-docgen-typescript").withDefaultConfig({
+  /*   propsParser: require("react-docgen-typescript").withDefaultConfig({
     propFilter: { skipPropsWithoutDoc: true }
-  }).parse,
+  }).parse, */
+  propsParser: require("react-docgen-typescript").withCustomConfig(
+    "./tsconfig.json",
+    {
+      propFilter: prop => {
+        if (prop.parent == null) {
+          return true;
+        }
+
+        return !prop.parent.fileName.includes("@types/react");
+      }
+    }
+  ).parse,
   skipComponentsWithoutExample: true,
   getExampleFilename(componentPath) {
     return componentPath.replace(/\.tsx?$/, ".md");
@@ -14,6 +26,5 @@ module.exports = {
   usageMode: "collapse",
   exampleMode: "collapse",
   require: [path.join(__dirname, "./src/styles/index.css")],
-
   ignore: ["**/*.test.*", "**/*.d.ts", "**/index.*"]
 };
